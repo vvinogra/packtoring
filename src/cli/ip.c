aee819e35dd35406217ed1220a6c3e2740c5a3e3
vvinogra <vvinogra@student.unit.ua>
2018-07-13 18:15:27 +0300

    fixed to line search

--- src/cli/ip.c
+++ src/cli/ip.c
@@ -1,35 +1,15 @@
 #include "header.h"
 
-static size_t	binarysearch(t_ipfile *ip_info, size_t len, uint32_t ip)
+static size_t	search(t_ipfile *ip_info, uint32_t ip)
 {
-	t_ipfile arr[len];
-	t_ipfile *fill_arr = ip_info;
+	t_ipfile *tmp = ip_info;
 
-	for (size_t i = 0; i < len; ++i)
+	while (tmp)
 	{
-		arr[i].ip = fill_arr->ip;
-		arr[i].pack_num = fill_arr->pack_num;
-		fill_arr = fill_arr->next;
+		if (tmp->ip == ip)
+			return (tmp->pack_num);
+		tmp = tmp->next;
 	}
-	if (len == 0)
-		return (0);
-	else if (arr[0].ip > ip)
-		return (0);
-	else if (arr[len - 1].ip < ip)
-		return (0);
-
-	size_t i = 0;
-	while (i < len)
-	{
-		size_t mid = i + (len - i) / 2;
-
-		if (ip <= arr[mid].ip)
-			len = mid;
-		else
-			i = mid + 1;
-	}
-	if (arr[len].ip == ip)
-		return (arr[len].pack_num);
 	return (0);
 }
 
@@ -49,7 +29,7 @@ void	show_ip_info(char *ip)
 		exit(1);
 	}
 	t_ipfile *ip_info = parse_ip_file(f);
-	size_t pack_num = binarysearch(ip_info, t_ipfile_getlen(&ip_info), check_ip.s_addr);
+	size_t pack_num = search(ip_info, check_ip.s_addr);
 	printf("%zu packets were received from this ip: %s\n", pack_num, ip);
 	clear_ip_parse_file(&ip_info);
 	fclose(f);
